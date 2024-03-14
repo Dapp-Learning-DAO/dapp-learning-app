32c198dd1e75af82e08fdada1cb5d67c2e839885
0x-stan <744428333@qq.com>
2024-03-14 18:08:49 +0800

    modify: bridege config

--- src/components/LiFiWidget/index.tsx
+++ src/components/LiFiWidget/index.tsx
@@ -3,6 +3,7 @@ import { LiFiWidget, WidgetConfig } from "@lifi/widget";
 import { useEffect, useMemo, useState } from "react";
 import { useConnect, useDisconnect, useSwitchChain } from "wagmi";
 import { useEthersSigner } from "hooks/useEthersSigner";
+import { chainsConf } from "provider/Web3Providers";
 
 export const Widget = () => {
   const { switchChainAsync } = useSwitchChain();
@@ -22,20 +23,30 @@ export const Widget = () => {
         connect: async () => {
           const signer = await connectAsync({
             connector: connectors[0],
+          }).catch((err) => {
+            console.error(err);
           });
           return signer as never;
         },
         disconnect: async () => {
-          await disconnectAsync();
+          await disconnectAsync().catch((err) => {
+            console.error(err);
+          });
         },
         switchChain: async (chainId: number) => {
-          await switchChainAsync({ chainId });
+          await switchChainAsync({ chainId }).catch((err) => {
+            console.error(err);
+          });
           if (signer) {
             return signer as never;
           } else {
             throw Error("No signer object is found after the chain switch.");
           }
         },
+      },
+      // disable BSC from being shown in the chains list
+      chains: {
+        allow: chainsConf.map((_c) => _c.id),
       },
       // It can be either default, expandable, or drawer
       variant: "expandable",
